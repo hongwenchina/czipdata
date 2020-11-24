ee382832cf8506cc473e4a4cc9ece24abc9d99a1
a76yyyy <56478790+a76yyyy@users.noreply.github.com>
2020-11-24 11:48:37 +0800

    规范化BAT

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -1,14 +1,14 @@
-@echo off
-if "%1" == "h" goto begin
+@ECHO OFF
+IF "%1" == "h" GOTO begin
 mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
 :begin
-@echo off 
-title =定时同步更新czipdata By A76YYYY
+@ECHO OFF 
+TITLE =定时同步更新czipdata By A76YYYY
 REM %DATE:~0,10%  2020/11/24
-set dd=%DATE:~0,10%
-set tt=%time:~0,8%
-set hour=%tt:~0,2%
-set ymd=%dd:/=-%
+SET dd=%DATE:~0,10%
+SET tt=%time:~0,8%
+SET hour=%tt:~0,2%
+SET ymd=%dd:/=-%
 REM change file directory
 CD /d %~dp0
 REM 判断log目录是否存在，如果不存在则创建
@@ -19,16 +19,16 @@ ECHO %logFolder%目录不存在，已创建该目录！
 MD %logFolder%
 )
 REM create log file
-set Log=log\Auto_Update_%ymd%.log
-(echo =======================================================
-echo          Starting automatic update czipdata
-echo =======================================================
+SET Log=log\Auto_Update_%ymd%.log
+(ECHO =======================================================
+ECHO          Starting automatic update czipdata
+ECHO =======================================================
 python ./IP_Sync/ip_Sync.py
-echo =======================================================
-echo          Starting automatic git commit push
-echo =======================================================
+ECHO =======================================================
+ECHO          Starting automatic git commit push
+ECHO =======================================================
 REM start git script 
-echo %~dp0
+ECHO %~dp0
 git add .
 git status -s
 git commit -m "定时同步 %dd:/=-% %tt%"
