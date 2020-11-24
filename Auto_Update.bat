476198a68a4c117ee27461477c3bf104df9af3af
a76yyyy <56478790+a76yyyy@users.noreply.github.com>
2020-11-24 10:02:12 +0800

    更新定时更新脚本

---
+++ Auto_Update.bat
@@ -0,0 +1,21 @@
+@echo off
+REM %DATE:~0,10%  2020/11/24
+set dd=%DATE:~0,10%
+set tt=%time:~0,8%
+set hour=%tt:~0,2%
+REM change file directory
+cd /d %~dp0
+echo =======================================================
+echo          Starting automatic update czipdata
+echo =======================================================
+python ./IP_Sync/ip_Sync.py
+echo =======================================================
+echo          Starting automatic git commit push
+echo =======================================================
+REM start git script 
+echo %~dp0
+git status
+git commit -m "定时同步 %dd:/=-% %tt%"
+git push origin main
+git push Gitee main
+pause
